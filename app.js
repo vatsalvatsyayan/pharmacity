12774557bd851ebe30b3f48a8e67887cedb2394e
Vatsal Vatsyayan <vatsaljackspicer@gmail.com>
2020-10-22 22:52:05 +0530

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var path = require('path');
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -51,6 +52,6 @@ app.get("/Vitamins",function(req,res){
 })
 
 
-app.listen(3000,function(req,res){
-    console.log("working fine");
-});
+app.listen(PORT,function(){
+    console.log("working");
+})
